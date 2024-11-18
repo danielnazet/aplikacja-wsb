6c25aee619d9b309b0b1d4efd357b4f35f07978b
Daniel Zaniewski <92151035+danielnazet@users.noreply.github.com>
2024-11-18 12:27:07 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+	return (
+		<div className="flex items-center justify-center min-h-screen bg-blue-500">
+			<h1 className="font-bold">Dashboard S & D</h1>
+		</div>
+	);
+}
+
+export default App;
